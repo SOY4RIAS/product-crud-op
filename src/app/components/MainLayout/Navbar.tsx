b402bd2f8ac1fde9ae70978c07d1cb5bef1711d2
Santiago Arias <sanx349@gmail.com>
2024-03-14 21:55:43 -0500

    fix: :bug: remove window that is causing issues with build

--- src/app/components/MainLayout/Navbar.tsx
+++ src/app/components/MainLayout/Navbar.tsx
@@ -8,22 +8,13 @@ import { Button } from '@/components/ui/button';
 export function Navbar() {
   const router = useRouter();
 
-  const canGoBack = () => window.history.length > 1;
-
   const handleBack = () => {
-    if (canGoBack()) {
-      router.back();
-    }
+    router.back();
   };
 
   return (
     <nav className="flex h-20 w-full items-center px-4 md:px-6 bg-white">
-      <Button
-        size="icon"
-        variant="ghost"
-        onClick={handleBack}
-        disabled={!canGoBack()}
-      >
+      <Button size="icon" variant="ghost" onClick={handleBack}>
         <CircleChevronLeft className="w-6 h-6 text-gray-300" />
       </Button>
       <div className="hidden md:flex items-center gap-4 ml-auto"></div>
