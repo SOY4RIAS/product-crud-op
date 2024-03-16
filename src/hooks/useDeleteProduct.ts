2aad4c920e7cc82b11114b7ba973f72cbd36c452
Santiago Arias <sanx349@gmail.com>
2024-03-15 22:52:58 -0500

    fix: add missing query key param

--- src/hooks/useDeleteProduct.ts
+++ src/hooks/useDeleteProduct.ts
@@ -2,19 +2,24 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 import { deleteProduct } from '@/services/products/products';
 import { GetProductsResponse } from '@/services/products';
+import { useSearchStore } from '@/providers/SearchStoreProvider';
 
 export const useDeleteProduct = () => {
+  const search = useSearchStore((state) => state.search);
   const queryClient = useQueryClient();
 
   return useMutation({
     mutationFn: deleteProduct,
     onSuccess: (data) => {
-      queryClient.setQueryData(['products'], (old: GetProductsResponse) => {
-        return {
-          ...old,
-          products: old.products.filter((product) => product.id !== data.id),
-        };
-      });
+      queryClient.setQueryData(
+        ['products', search],
+        (old: GetProductsResponse) => {
+          return {
+            ...old,
+            products: old.products.filter((product) => product.id !== data.id),
+          };
+        }
+      );
     },
   });
 };
